bc3a461cdf7c38484cfed55d771e085c6de47f74
VirusBlITZ <58221423+VirusBLITZ@users.noreply.github.com>
2023-06-02 07:24:02 +0200

    render ips from scan_peers function

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
   <main>
     <h1>Peer-Drop</h1>
+    <h3>clients</h3>
+    <ul>
+      <li v-for="client in clients">{{ client }}</li>
+    </ul>
   </main>
 </template>
+
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api'
+
+const clients = ref([]);
+
+const scan_peers = async () => {
+  clients.value = await invoke('scan_peers')
+}
+scan_peers()
+</script>
+
+<style>
+body {
+  background-color: #313030;
+  font-family: sans-serif;
+  color: white;
+}
+</style>
